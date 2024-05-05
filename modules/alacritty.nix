fef63e47c197480f0771a36793673294b4ac6881
David Jelenc <david.jelenc@fri.uni-lj.si>
2024-05-05 12:44:16 +0200

    idea@329: uses HM settings for alacritty

--- modules/alacritty.nix
+++ modules/alacritty.nix
@@ -1,23 +1,30 @@
 { inputs, pkgs, ... }: {
   # install
-  home.packages = with pkgs; [ alacritty zellij ];
+  # home.packages = with pkgs; [ alacritty zellij ];
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window = {
+        opacity = 0.75;
+        blur = true;
+        padding = {
+          x = 5;
+          y = 5;
+        };
+        dynamic_padding = true;
+      };
 
-  # configuration
-  home.file.".config/alacritty/alacritty.toml".text = ''
-    [window]
-    opacity = 0.75
-    blur = true
-    padding = { x = 5, y = 5 }
-    dynamic_padding = true
+      env = { TERM = "xterm-256color"; };
 
-    [env]
-    TERM = "xterm-256color"
+      scrolling.history = 100000;
 
-    [scrolling]
-    history = 100000
-
-    [font]
-    normal = { family = "CaskaydiaMono Nerd Font", style = "Regular" }
-    size = 17.0
-  '';
+      font = {
+        normal = {
+          family = "CaskaydiaMono Nerd Font";
+          style = "Regular";
+        };
+        size = 17.0;
+      };
+    };
+  };
 }
