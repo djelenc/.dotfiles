913263953bc9ba0032688b8e269c1755fbd74ce8
David Jelenc <david.jelenc@fri.uni-lj.si>
2024-10-03 11:06:42 +0200

    Configures monitors

--- modules/hyprland-desktop/config.nix
+++ modules/hyprland-desktop/config.nix
@@ -1,8 +1,13 @@
 { config, lib, pkgs, inputs, ... }: {
   # hyprland config
   wayland.windowManager.hyprland.settings = {
-    # TODO: this may change on every host
-    monitor = ",preferred,auto,1.25";
+    # monitor = ",preferred,auto,1.25";
+    monitor = [
+      # name, resolution, position, scale
+      "eDP-1, preferred, auto, 1.25, vrr, 1"
+      "desc:AOC Q27P1B GNXL7HA167657, preferred, auto-up, 1"
+      ", preferred, auto, 1, mirror, eDP-1"
+    ];
 
     xwayland = { force_zero_scaling = true; };
 
@@ -14,10 +19,7 @@
       "${pkgs.nextcloud-client}/bin/nextcloud"
     ];
 
-    env = [
-      # "XCURSOR_SIZE,32"
-      "GDK_SCALE,1.25"
-    ];
+    env = [ "GDK_SCALE,1.25" ];
 
     input = {
       kb_layout = "us,si";
