a9f513579004e52d666baf68bf19bbe8340ce88c
David Jelenc <david.jelenc@fri.uni-lj.si>
2026-07-07 00:03:07 +0200

    hyprland: keep static lua settings in nix

--- modules/hyprland-desktop/config.nix
+++ modules/hyprland-desktop/config.nix
@@ -1,8 +1,6 @@
 {
-  config,
   lib,
   pkgs,
-  inputs,
   ...
 }:
 let
@@ -58,237 +56,139 @@ let
   '';
 in
 {
-  # hyprland config
+  home.packages = [
+    hyprland-switch-down
+    hyprland-switch-up
+    hyprland-move-down
+    hyprland-move-up
+  ];
+
+  # Static Hyprland options that map cleanly through Home Manager's Lua generator.
+  # Dynamic/runtime pieces live in ./lua/*.lua and are loaded through extraLuaFiles.
   wayland.windowManager.hyprland.settings = {
-    xwayland = {
-      force_zero_scaling = true;
+    config = {
+      xwayland = {
+        force_zero_scaling = true;
+      };
+
+      # Workaround for missing cursors on other monitors
+      cursor = {
+        no_hardware_cursors = true;
+      };
+
+      input = {
+        kb_layout = "us,si";
+        kb_options = "grp:win_space_toggle";
+        numlock_by_default = true;
+
+        follow_mouse = 2;
+        touchpad = {
+          natural_scroll = false;
+        };
+        sensitivity = 0;
+        repeat_rate = 50; # 25
+        repeat_delay = 300; # 600
+      };
+
+      general = {
+        gaps_in = 4;
+        gaps_out = 4;
+        border_size = 2;
+        col = {
+          active_border = lib.mkDefault {
+            colors = [
+              "rgba(33ccffee)"
+              "rgba(00ff99ee)"
+            ];
+            angle = 45;
+          };
+          inactive_border = lib.mkDefault "rgba(595959aa)";
+        };
+        layout = "dwindle";
+        allow_tearing = false;
+      };
+
+      decoration = {
+        rounding = 8;
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 1;
+        };
+      };
+
+      animations = {
+        enabled = true;
+      };
+
+      dwindle = {
+        preserve_split = true;
+      };
+
+      misc = {
+        force_default_wallpaper = 0;
+        disable_hyprland_logo = true;
+        on_focus_under_fullscreen = 1;
+      };
     };
-
-    exec-once = [
-      "${pkgs.waybar}/bin/waybar"
-      "${pkgs.wlsunset}/bin/wlsunset -l 46 -L 14.5"
-      "${pkgs.networkmanagerapplet}/bin/nm-applet --indicator"
-      "${pkgs.blueman}/bin/blueman-applet"
-      "${pkgs.nextcloud-client}/bin/nextcloud"
-      "${pkgs.wl-clipboard}/bin/wl-paste --type text --watch cliphist store # Stores only text data"
-      "${pkgs.wl-clipboard}/bin/wl-paste --type image --watch cliphist store # Stores only image data"
-      "${inputs.pyprland.packages.${pkgs.stdenv.hostPlatform.system}.pyprland}/bin/pypr"
-    ];
-
-    #   # DOMA
-    #   "desc:AOC Q27P1B GNXL7HA167657, preferred, auto-center-up, 1"
-    #   "desc:Philips Consumer Electronics Company 231PQPY UHB1430018671, preferred, auto-center-right, 1, transform, 1"
 
     monitor = [
       # Laptop: below AOC, centered under it.
       # Adjust 512 if it is not visually centered.
-      "eDP-1, preferred, 512x1440, 1.25, vrr, 1"
+      {
+        output = "eDP-1";
+        mode = "preferred";
+        position = "512x1440";
+        scale = 1.25;
+        vrr = 1;
+      }
 
       # AOC: main anchor, top-left of layout.
-      "desc:AOC Q27P1B GNXL7HA167593, preferred, 0x0, 1"
+      {
+        output = "desc:AOC Q27P1B GNXL7HA167593";
+        mode = "preferred";
+        position = "0x0";
+        scale = 1;
+      }
 
-      # Philips: to the right of AOC, rotated.
-      "desc:Dell Inc. DELL U2412M 0FFXD4136Y1L, preferred, 2560x0, 1, transform, 1"
+      # Dell: to the right of AOC, rotated.
+      {
+        output = "desc:Dell Inc. DELL U2412M 0FFXD4136Y1L";
+        mode = "preferred";
+        position = "2560x0";
+        scale = 1;
+        transform = 1;
+      }
 
       # Fallback/mirror
-      ", preferred, auto, 1, mirror, eDP-1"
+      {
+        output = "";
+        mode = "preferred";
+        position = "auto";
+        scale = 1;
+        mirror = "eDP-1";
+      }
     ];
-
-    # Workaround for missing cursors on other monitors
-    cursor = {
-      no_hardware_cursors = true;
-    };
 
     env = [
-      # "GDK_SCALE,1.25"
-      "GDK_SCALE,1.0"
-      "HYPRCURSOR_SIZE,30"
-      "XCURSOR_SIZE,30"
+      {
+        _args = [
+          "GDK_SCALE"
+          "1.0"
+        ];
+      }
+      {
+        _args = [
+          "HYPRCURSOR_SIZE"
+          "30"
+        ];
+      }
+      {
+        _args = [
+          "XCURSOR_SIZE"
+          "30"
+        ];
+      }
     ];
-
-    input = {
-      kb_layout = "us,si";
-      kb_options = "grp:win_space_toggle";
-      numlock_by_default = true;
-
-      follow_mouse = 2;
-      touchpad = {
-        natural_scroll = "no";
-      };
-      sensitivity = 0;
-      repeat_rate = 50; # 25
-      repeat_delay = 300; # 600
-    };
-
-    general = {
-      gaps_in = 4;
-      gaps_out = 4;
-      border_size = 2;
-      "col.active_border" = lib.mkDefault "rgba(33ccffee) rgba(00ff99ee) 45deg";
-      "col.inactive_border" = lib.mkDefault "rgba(595959aa)";
-      layout = "dwindle";
-      allow_tearing = false;
-    };
-
-    decoration = {
-      rounding = 8;
-      blur = {
-        enabled = true;
-        size = 3;
-        passes = 1;
-      };
-    };
-
-    animations = {
-      enabled = "yes";
-      bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
-      animation = [
-        "windows, 1, 7, myBezier"
-        "windowsOut, 1, 7, default, popin 80%"
-        "border, 1, 10, default"
-        "borderangle, 1, 8, default"
-        "fade, 1, 7, default"
-        "workspaces, 1, 2, default, slidevert"
-      ];
-    };
-
-    dwindle = {
-      preserve_split = "yes";
-    };
-
-    # hyprctl dispatch split-cycleworkspaces next
-    gesture = [
-      "3, down, dispatcher, exec, ${hyprland-switch-up}/bin/hyprland-switch-up"
-      "3, up, dispatcher, exec, ${hyprland-switch-down}/bin/hyprland-switch-down"
-      "3, down, mod:SUPER, dispatcher, split-cycleworkspaces, prev"
-      "3, up, mod:SUPER, dispatcher, split-cycleworkspaces, next"
-    ];
-
-    misc = {
-      force_default_wallpaper = 0;
-      disable_hyprland_logo = true;
-      on_focus_under_fullscreen = 1;
-    };
-
-    "$mainMod" = "SUPER";
-
-    bind = [
-      "$mainMod, T, exec, alacritty"
-      "$mainMod, Q, killactive,"
-      "$mainMod, M, fullscreen, 1"
-      "$mainMod SHIFT, Q, exit,"
-      "$mainMod SHIFT, R, exec, hyprctl reload"
-      "$mainMod, F, exec, nautilus"
-      "$mainMod, G, togglefloating,"
-      "$mainMod, O, layoutmsg, togglesplit"
-      # "$mainMod, O, togglesplit,"
-      "$mainMod, h, movefocus, l"
-      "$mainMod, l, movefocus, r"
-      "$mainMod, k, movefocus, u"
-      "$mainMod, j, movefocus, d"
-
-      # scratchpad
-      "$mainMod, grave, togglespecialworkspace, scratch"
-      # drop from scratch to active workspace
-      ''
-        $mainMod SHIFT, grave, exec, hyprctl dispatch movetoworkspace "$(hyprctl activeworkspace -j | jq -r '.id')"
-      ''
-
-      # change keyboard
-      "$mainMod, SPACE, exec, hyprctl switchxkblayout at-translated-set-2-keyboard next"
-
-      # configuration: monitors side-by-side, workspaces switch up/down
-      "$mainMod CONTROL, j, exec, ${hyprland-switch-down}/bin/hyprland-switch-down"
-      "$mainMod CONTROL, k, exec, ${hyprland-switch-up}/bin/hyprland-switch-up"
-      "$mainMod SHIFT, j, exec, ${hyprland-move-down}/bin/hyprland-move-down"
-      "$mainMod SHIFT, k, exec, ${hyprland-move-up}/bin/hyprland-move-up"
-      "$mainMod SHIFT, h, movewindow, l"
-      "$mainMod SHIFT, l, movewindow, r"
-      # TODO
-      # "$mainMod SHIFT, n, split-changemonitor, next"
-      # "$mainMod SHIFT, p, split-changemonitor, prev"
-
-      # TODO: should be MRU not cycling
-      # TODO: should include apps on all monitors
-      # "$mainMod, Tab, cyclenext, visible hist" # <-- problem, ko je okno maksimizirano
-      "$mainMod, Tab, cyclenext, "
-      "$mainMod, Tab, alterzorder, top"
-
-      ", xf86audiomute, exec, swayosd-client --output-volume mute-toggle"
-      ", xf86audiomicmute, exec, swayosd-client --input-volume mute-toggle"
-      "$mainMod, escape, exec, hyprlock"
-
-      # zooming
-      "$mainMod, Z, exec, pypr zoom ++0.5"
-      "$mainMod SHIFT, Z, exec, pypr zoom"
-    ];
-
-    # when held, repeat key
-    binde = [
-      ", xf86audioraisevolume, exec, swayosd-client --output-volume raise"
-      ", xf86audiolowervolume, exec, swayosd-client --output-volume lower"
-      ", xf86monbrightnessup, exec, swayosd-client --brightness raise"
-      ", xf86monbrightnessdown, exec, swayosd-client --brightness lower"
-    ];
-
-    bindm = [
-      "$mainMod, mouse:272, movewindow"
-      "$mainMod, mouse:273, resizewindow"
-    ];
-
-    # lock on lid-open
-    bindl = [ ",switch:off:Lid Switch, exec, hyprlock" ];
-
-    # execute on release
-    bindr = "$mainMod, SUPER_L, exec, pkill fuzzel || fuzzel";
-
-    # TO-REMOVE: Old rules, check if new work OK, delete then
-    # windowrulev2 = [
-    #   "float,class:^(com.saivert.pwvucontrol)$"
-    #   "float,class:^(com.nextcloud.desktopclient.nextcloud)$"
-    #   "float,class:(blueman)"
-    #   "float,class:^(vlc)$"
-    #   "float,class:^(org.keepassxc.KeePassXC)$"
-    #   "float,initialTitle:(All Files)"
-    #   "float,initialTitle:.*wants to save$"
-    #   "float,initialTitle:^(Print)$"
-    #   "move 45% 2.9%,class:^(gsimplecal)$"
-    #   "float,initialTitle:.*Welcome to PyCharm"
-    #   "float, title:^Calc$" # calc always floats
-    # ];
-
-    # To be used when upgrading
-    windowrule = [
-      "float on, match:class ^(com.saivert.pwvucontrol)$"
-      "float on, match:class ^(com.nextcloud.desktopclient.nextcloud)$"
-      "float on, match:class blueman"
-      "float on, match:class ^(vlc)$"
-      "float on, match:class ^(org.keepassxc.KeePassXC)$"
-
-      # dialogs (titles often contain spaces; that's fine as a regex)
-      "float on, match:initial_title (All Files)"
-      "float on, match:initial_title .*wants to save$"
-      "float on, match:initial_title ^(Print)$"
-      "float on, match:initial_title .*Welcome to PyCharm"
-
-      # gsimplecal: old "move 45% 2.9%" often breaks with the new expression parser.
-      # Use monitor-local expressions instead:
-      "float on, move (monitor_w*0.45) (monitor_h*0.029), match:class ^(gsimplecal)$"
-
-      # calc always floats
-      "float on, match:title ^Calc$"
-    ];
-
   };
-
-  # wayland.windowManager.hyprland.extraConfig = ''
-  #   plugin {
-  #     split-monitor-workspaces {
-  #       count = 5
-  #       keep_focused = 1
-  #       enable_notifications = 0
-  #       enable_persistent-workspaces = 1
-  #     }
-  #   }
-  # '';
 }
