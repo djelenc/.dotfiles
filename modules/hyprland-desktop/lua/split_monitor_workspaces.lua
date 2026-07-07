7e0440288033440abce9929b762a151c69d1ddb7
David Jelenc <david.jelenc@fri.uni-lj.si>
2026-07-07 09:51:26 +0200

    hyprland: split workspaces monitor priority

--- modules/hyprland-desktop/lua/split_monitor_workspaces.lua
+++ modules/hyprland-desktop/lua/split_monitor_workspaces.lua
@@ -22,6 +22,9 @@ smw.setup({
   -- The first connected AOC becomes the primary workspace range, the side monitor second,
   -- and the laptop panel third when it is part of the layout.
   monitor_priority = {
+    -- Laptop
+    "eDP-1",
+
     -- Home
     "desc:AOC Q27P1B GNXL7HA167657",
     "desc:Philips Consumer Electronics Company 231PQPY UHB1430018671",
@@ -29,9 +32,6 @@ smw.setup({
     -- FRI
     "desc:AOC Q27P1B GNXL7HA167593",
     "desc:Dell Inc. DELL U2412M 0FFXD4136Y1L",
-
-    -- Laptop
-    "eDP-1",
   },
 })
 
